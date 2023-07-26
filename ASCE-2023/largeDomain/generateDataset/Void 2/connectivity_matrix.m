80e27801a4b4d57794319d9b01a8239b7de5af71
Shashwat <shashwatmjn@gmail.com>
2023-07-25 21:06:30 -0700

    Updated large domain Void 2 generate dataset

--- ASCE-2023/largeDomain/generateDataset/Void 2/connectivity_matrix.m
+++ ASCE-2023/largeDomain/generateDataset/Void 2/connectivity_matrix.m
@@ -7,19 +7,20 @@ EBC_nn1 =[]; EBC_nn2 =[]; Connectivity = [];
 for i = 1:(NV)
     for j = 1:(NH)
         e = (i-1)*NH +j;
-        x_coord_ele(e) = (j-1)*h + h/2 ;   %x-coordinate of center of each element
-        y_coord_ele(e) = (i-1)*h + h/2 ;   %y-coordinate of center of each element
+        x_coord_ele(e) = (j-1)*h + h/2 ;   % x-coordinate of center of each element
+        y_coord_ele(e) = (i-1)*h + h/2 ;   % y-coordinate of center of each element
         if (j == 1)
             Connectivity(e,1) = (2*(i-1)) * (2*NH+1)+1;
-            Connectivity(e,2) = Connectivity(e,1)+1; 
-            Connectivity(e,3) = Connectivity(e,2)+1;           
+            Connectivity(e,2) = Connectivity(e,1)+1;
+            Connectivity(e,3) = Connectivity(e,2)+1;
             Connectivity(e,4) = (2*i-1)  * (2*NH+1)+1;
             Connectivity(e,5) = Connectivity(e,4)+1;
-            Connectivity(e,6) = Connectivity(e,5)+1;            
+            Connectivity(e,6) = Connectivity(e,5)+1;
             Connectivity(e,7) = (2*i) * (2*NH+1)+1;
             Connectivity(e,8) = Connectivity(e,7)+1;
             Connectivity(e,9) = Connectivity(e,8)+1;
-            %Essential boundary condition array: when the LHS(left hand side) is fixed.
+            
+            % Essential boundary condition array: when the LHS(left hand side) is fixed.
             if (i == 1) %the bottom row.
                 EBC_nn1 = [EBC_nn1,[Connectivity(e,1),Connectivity(e,4),Connectivity(e,7)]];
             else
@@ -46,11 +47,7 @@ xy_coord_ele = [x_coord_ele; y_coord_ele]';
 
 EBC_nn3 = [1:2*NH+1]; %Bottom side fixed.
 
-% % EBC_nn1 = left side; EBC_nn2 = right side; EBC_nn3 = bottom
-%EBC_nn = unique([EBC_nn1,EBC_nn2,EBC_nn3]);     %left, right, and bottom fixed!
-
 EBC_nn = unique([EBC_nn2]);                     %only right side fixed
-
 
 %% Double up the size of EBC_nn array (considering both x and y components of zero displacement)
 temp = length(EBC_nn);
